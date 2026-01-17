58406c62a0921fa3095f54e02ff7890e4922a599
Abdulhamid <weheliye.abdul@gmail.com>
2026-01-16 19:27:47 -0500

    Added route.js

---
+++ route.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const server = http.createServer((req,res) =>{
+    //Creates backend
+    if(req.url ==='/home'){
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        return res.end('Home Page');
+    }
+     if(req.url ==='/about'){
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        return res.end('About Page');
+    }
+
+    res.writeHead(404,{'content-type':'text/plain'});
+    return res.end('404 - Page not Found');
+})
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server is running at: http://localhost:${PORT}`);
+})
